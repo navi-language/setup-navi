c55843164449d5e4c78c7db41fd15307fdf766e1
Jason Lee <huacnlee@gmail.com>
2023-11-01 12:02:53 +0800

    Update install

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
 import * as core from '@actions/core';
-import * as tc from '@actions/tool-cache';
+import { exec } from '@actions/exec';
 import * as cache from './cache';
 
 export async function run() {
   try {
-    let versionSpec = core.getInput('navi-version', { required: true });
+    let versionSpec = core.getInput('navi-version');
+    if (versionSpec.startsWith('v')) {
+      versionSpec = versionSpec.slice(1);
+    }
 
     // check in the VMs cache first
     let toolPath: string = await cache.find(versionSpec);
 
     if (!toolPath) {
-      await tc.downloadTool('https://navi-lang.org/install');
+      if (versionSpec) {
+        await exec(
+          `curl -sSL https://navi-lang.org/install | sh -s -- v${versionSpec}`
+        );
+      } else {
+        // download latest
+        await exec('curl -L https://navi-lang.org/install | sh');
+      }
     }
 
     core.addPath(toolPath);
