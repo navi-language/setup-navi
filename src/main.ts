c5f440f6ad2240e2e204cd369390806142ba42ab
Jason Lee <huacnlee@gmail.com>
2023-12-12 19:42:55 +0800

    Install navi script by use `bash`

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import * as core from "@actions/core";
-import { execSync } from "child_process";
+import * as core from '@actions/core';
+import { execSync } from 'child_process';
 
 export async function run() {
   try {
-    let versionSpec = core.getInput("navi-version");
-    if (!versionSpec.startsWith("v")) {
+    let versionSpec = core.getInput('navi-version');
+    if (!versionSpec.startsWith('v')) {
       // 1.0 -> v1.0
       if (versionSpec.match(/\d+\./)) {
         versionSpec = `v${versionSpec}`;
@@ -14,11 +14,11 @@ export async function run() {
     // check in the VMs cache first
     if (versionSpec) {
       execSync(
-        `curl -sSL https://navi-lang.org/install | sh -s -- ${versionSpec}`
+        `curl -sSL https://navi-lang.org/install | bash -s -- ${versionSpec}`
       );
     } else {
       // download latest
-      execSync("curl -L https://navi-lang.org/install | sh");
+      execSync('curl -L https://navi-lang.org/install | bash');
     }
 
     let toolPath = `${process.env.HOME}/.navi`;
