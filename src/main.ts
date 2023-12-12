5c9867f96ae6916921ae954eb29a2bcd0382a300
Jason Lee <huacnlee@gmail.com>
2023-12-12 11:05:28 +0800

    Update to use `$HOME/.navi` install path.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import * as core from '@actions/core';
-import { execSync } from 'child_process';
-import * as cache from './cache';
+import * as core from "@actions/core";
+import { execSync } from "child_process";
 
 export async function run() {
   try {
-    let versionSpec = core.getInput('navi-version');
-    if (!versionSpec.startsWith('v')) {
+    let versionSpec = core.getInput("navi-version");
+    if (!versionSpec.startsWith("v")) {
       // 1.0 -> v1.0
       if (versionSpec.match(/\d+\./)) {
         versionSpec = `v${versionSpec}`;
@@ -13,22 +12,18 @@ export async function run() {
     }
 
     // check in the VMs cache first
-    let toolPath: string = await cache.find(versionSpec || 'latest');
-
-    if (!toolPath) {
-      if (versionSpec) {
-        execSync(
-          `curl -sSL https://navi-lang.org/install | sh -s -- ${versionSpec}`
-        );
-      } else {
-        // download latest
-        execSync('curl -L https://navi-lang.org/install | sh');
-      }
+    if (versionSpec) {
+      execSync(
+        `curl -sSL https://navi-lang.org/install | sh -s -- ${versionSpec}`
+      );
+    } else {
+      // download latest
+      execSync("curl -L https://navi-lang.org/install | sh");
     }
 
-    execSync(`navi -V`);
-
+    let toolPath = `${process.env.HOME}/.navi`;
     core.addPath(toolPath);
+    execSync(`navi -V`);
   } catch (error) {
     // unhandled
     core.setFailed(error.message);
